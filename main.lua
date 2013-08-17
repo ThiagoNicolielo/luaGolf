56969583ffdd3e72f4c1dfd89ca14074f45d5eba
roothiago <roothiago@192.168.1.67>
2013-08-17 18:21:11 -0300

    commit inicial

--- main.lua
+++ main.lua
@@ -13,19 +13,65 @@ local physics = require("physics")
 local gameUI = require("gameUI")
 display.setStatusBar ( display.HiddenStatusBar)
 
+function constroiCaixa(hasBotao)
+    local obCaixa = display.newGroup()
+    local parteE1 = display.newRect(obCaixa, 00, 195, 130, 5)
+    local parteE2 = display.newRect(obCaixa, 00, 200, 90, 20)
+    local parteE3 = display.newRect(obCaixa, 00, 220, 130, 5)
+    local parteD1 = display.newRect(obCaixa, 190, 195, 130, 5)
+    local parteD2 = display.newRect(obCaixa, 230, 200, 90, 20)
+    local parteD3 = display.newRect(obCaixa, 190, 220, 130, 5)
+    parteMovel = display.newRect(obCaixa, 90, 200, 140, 20)
+    parteMovel:setFillColor(255, 0, 0, 150)
+    if (hasBotao) then
+        botao = display.newRect(obCaixa, 200, 222, 40, 10)
+        botao:setFillColor(255, 0, 0, 255)
+        botao.isOpen = false
+    end
+    physics.addBody(parteE1, "static")
+    physics.addBody(parteE2, "static")
+    physics.addBody(parteE3, "static")
+    physics.addBody(parteD1, "static")
+    physics.addBody(parteD2, "static")
+    physics.addBody(parteD3, "static")
+    physics.addBody(parteMovel, "static")
+    physics.addBody(botao, "static")
+    physics.addBody(obCaixa, "static")
+end
+
+
+local function abrePorta()
+    parteMovel:setReferencePoint(display.CenterLeftReferencePoint)
+            print("aa")
+    while (not isOpen) do
+        if (parteMovel.x < 190 and not isOpen) then
+            parteMovel.x = parteMovel.x + 1
+        else
+            isOpen = true
+        end
+    end
+        
+
+end
+
 
 physics.start()
 local bg = display.newImage ("grassbg.png", true)
 --physics.addBody(bg)
 bg.x = display.contentCenterX
 bg.y = display.contentCenterY
+--buraco
 local hole = display.newCircle (_W/2, 100, 17)
 hole:setFillColor(0,0,0)
+--bola
 local ball = display.newCircle (_W/2, 400, 10)
 physics.addBody(ball, {density = 1, friction = 1})
-local ob1 = display.newRect (0, 250, 200, 20)
-physics.addBody (ob1, "static")
 physics.setGravity(0,0)
+--obstaculo barra
+--local ob1 = display.newRect (0, 150, 200, 20)
+--physics.addBody (ob1, "static")
+--obstaculo caixa
+constroiCaixa(true)
 
 system.activate( "multitouch" )
 local function dragBody( event )
@@ -67,9 +113,18 @@ function ballEnteredHole()
 		goodOne = display.newText ("You Win!", _W/2-20, _H/2)
 	end
 end
+        
+function botaoApertado()
+    botao:setReferencePoint(display.BottomCenterReferencePoint)
+    if (math.abs(botao.x - ball.x) < 20 and math.abs(botao.y - ball.y) < 30) then
+                abrePorta()
+    end
+
+end
 ball:addEventListener( "touch", dragBody )
 Runtime:addEventListener( "enterFrame", ballFriction)
 Runtime:addEventListener( "enterFrame", ballEnteredHole)
+Runtime:addEventListener("enterFrame", botaoApertado)
 
 
 
