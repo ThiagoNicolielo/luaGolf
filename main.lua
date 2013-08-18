2119374180fc265fb46e4bf585651787e4d282a2
roothiago <roothiago@192.168.1.67>
2013-08-18 13:56:30 -0300

    banco areia

--- main.lua
+++ main.lua
@@ -11,20 +11,22 @@ local _W = display.contentWidth
 local _H = display.contentHeight
 local physics = require("physics")
 local gameUI = require("gameUI")
+local ball
+local taxaDesaceleracao = 1
 display.setStatusBar ( display.HiddenStatusBar)
 
 function constroiCaixa(hasBotao)
     local obCaixa = display.newGroup()
-    local parteE1 = display.newRect(obCaixa, 00, 195, 130, 5)
-    local parteE2 = display.newRect(obCaixa, 00, 200, 90, 20)
-    local parteE3 = display.newRect(obCaixa, 00, 220, 130, 5)
-    local parteD1 = display.newRect(obCaixa, 190, 195, 130, 5)
-    local parteD2 = display.newRect(obCaixa, 230, 200, 90, 20)
-    local parteD3 = display.newRect(obCaixa, 190, 220, 130, 5)
-    parteMovel = display.newRect(obCaixa, 90, 200, 140, 20)
-    parteMovel:setFillColor(255, 100, 0, 150)
+    local parteE1 = display.newRect(obCaixa, 00, 125, 130, 5)
+    local parteE2 = display.newRect(obCaixa, 00, 130, 90, 20)
+    local parteE3 = display.newRect(obCaixa, 00, 150, 130, 5)
+    local parteD1 = display.newRect(obCaixa, 190, 125, 130, 5)
+    local parteD2 = display.newRect(obCaixa, 230, 130, 90, 20)
+    local parteD3 = display.newRect(obCaixa, 190, 150, 130, 5)
+    parteMovel = display.newRect(obCaixa, 90, 130, 140, 20)
+    parteMovel:setFillColor(220, 150, 0, 255)
     if (hasBotao) then
-        botao = display.newRect(obCaixa, 200, 222, 40, 10)
+        botao = display.newRect(obCaixa, 200, 152, 40, 10)
         botao:setFillColor(255, 0, 0, 255)
         botao.isOpen = false
     end
@@ -50,6 +52,49 @@ local function abrePorta()
 
 end
 
+function constroiBancoAreia(x, y)
+    local areia = display.newImage("areia.png")
+    areia:setReferencePoint(display.TopRightReferencePoint)
+    areia.x = x --130
+    areia.y = y --240
+    ball:toFront()
+    local limite1 = display.newRect(35, 250, 15, 70)
+    limite1.isVisible = false
+    local limite2 = display.newRect(50, 240, 30, 100)
+    limite2.isVisible = false
+    local limite3 = display.newRect(80, 260, 15, 80)
+    limite3.isVisible = false
+    local limite4 = display.newRect(95, 280, 15, 55)
+    limite4.isVisible = false  
+    local limite5 = display.newRect(110, 290, 15, 40)
+    limite5.isVisible = false
+end
+
+function alteraFriccao()
+    xbegin, ybegin, xend, yend = 35, 240, 125, 340
+--    local limiteExterno = display.newRect(xbegin, ybegin, xend - xbegin, yend - ybegin)
+--    limiteExterno:setFillColor(255, 255, 255, 100)
+    -- verifica se a bola esta na area maior (quadrada)
+    if (ball.x > xbegin and ball.x < xend and ball.y > ybegin and ball.y < yend) then
+        --verifica se a bola esta dentro da area mais aproximada da figura irregular
+            xbeginl1, ybeginl1, xendl1, yendl1 = 35, 250, 50, 320
+            xbeginl2, ybeginl2, xendl2, yendl2 = 50, 240, 80, 340
+            xbeginl3, ybeginl3, xendl3, yendl3 = 80, 260, 95, 340
+            xbeginl4, ybeginl4, xendl4, yendl4 = 95, 280, 110, 335
+            xbeginl5, ybeginl5, xendl5, yendl5 = 110, 290, 125, 330
+        if (ball.x>xbeginl1 and ball.x<xendl1 and ball.y>ybeginl1 and ball.y<yendl1 or 
+            ball.x>xbeginl2 and ball.x<xendl2 and ball.y>ybeginl2 and ball.y<yendl2 or 
+            ball.x>xbeginl3 and ball.x<xendl3 and ball.y>ybeginl3 and ball.y<yendl3 or 
+            ball.x>xbeginl4 and ball.x<xendl4 and ball.y>ybeginl4 and ball.y<yendl4 or 
+            ball.x>xbeginl5 and ball.x<xendl5 and ball.y>ybeginl5 and ball.y<yendl5) then
+                taxaDesaceleracao = 2
+        else
+            taxaDesaceleracao = 1
+        end
+    else
+        taxaDesaceleracao = 1
+    end
+end
 
 physics.start()
 local bg = display.newImage ("grassbg.png", true)
@@ -60,7 +105,7 @@ bg.y = display.contentCenterY
 local hole = display.newCircle (_W/2, 100, 17)
 hole:setFillColor(0,0,0)
 --bola
-local ball = display.newCircle (_W/2, 400, 10)
+ball = display.newCircle (_W/2, 400, 10)
 physics.addBody(ball, {density = 1, friction = 1})
 physics.setGravity(0,0)
 --obstaculo barra
@@ -68,6 +113,9 @@ physics.setGravity(0,0)
 --physics.addBody (ob1, "static")
 --obstaculo caixa
 constroiCaixa(true)
+--obstaculo areia
+constroiBancoAreia(130, 240)
+
 
 system.activate( "multitouch" )
 local function dragBody( event )
@@ -77,6 +125,7 @@ local function dragBody( event )
 	--gameUI.dragBody( event, { maxForce=400, frequency=5, dampingRatio=0.2 } ) -- slow, elastic dragging
 	--gameUI.dragBody( event, { maxForce=20000, frequency=1000, dampingRatio=1.0, center=true } ) -- very tight dragging, snaps to object center
 end
+
 function ballFriction()
 	ball.angularVelocity=0
 	local vx, vy=ball:getLinearVelocity()
@@ -96,8 +145,9 @@ function ballFriction()
         end
 
 
-
-
+        
+            newvx=newvx/taxaDesaceleracao
+            newvy=newvy/taxaDesaceleracao
 	    ball:setLinearVelocity(newvx,newvy)
 	end
 	
@@ -121,6 +171,7 @@ ball:addEventListener( "touch", dragBody )
 Runtime:addEventListener( "enterFrame", ballFriction)
 Runtime:addEventListener( "enterFrame", ballEnteredHole)
 Runtime:addEventListener("enterFrame", botaoApertado)
+Runtime:addEventListener("enterFrame", alteraFriccao)
 
 
 
